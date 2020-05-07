cc76cded8e8ed3e5f6b52a1970e41d31a6532bd8
thieleke <jeff.thieleke@gmail.com>
2020-05-07 13:37:31 -0500

    Autoformat/whitespace changes

--- DayClock.ino
+++ DayClock.ino
@@ -1,10 +1,10 @@
 /*
- * Default Pinouts
- *    * Servo:  GPIO Pin 13 
+   Default Pinouts
+ *    * Servo:  GPIO Pin 13
  *    * DHT: 5V, GND, GPIO Pin 14
  *    * LCD: 5V, GND, SDA -> I2C SDA, SCL -> IC2 SCL
  *    * MH-Z19B: RX -> TX2, TX -> RX2
- */ 
+*/
 
 /* Constants that you'll definitely want to update */
 //#define DISPLAY_CELSIUS                         // Uncomment if you want the LCD and HTTP displays to default to Celsius temperature instead of Fahrenheit
@@ -13,7 +13,7 @@
 const char *ssid               = "YOUR_SSID";     // Your WiFi SSID
 const char *password           = "YOUR_PASSWORD"; // Your WiFi password
 const char* ntpServer          = "pool.ntp.org";  // There are also region-specific addresses such as "us.pool.ntp.org"
-const long  gmtOffset_sec      = -21600;          // Non-daylight saving time offset from UTC, in minutes (-18000 = US Eastern Standard Time, -21600 = US Central Standard Time, etc.)
+const long  gmtOffset_sec      = -21600;          // Non-daylight saving time offset from UTC, in seconds (-18000 = US Eastern Standard Time, -21600 = US Central Standard Time, etc.)
 const int   daylightOffset_sec = 3600;            // Number of minutes offset for daylight saving time (0 if daylight savings time if not applicable)
 
 #define SERVO_PIN  13
@@ -97,19 +97,19 @@ void setup() {
   lcd.backlight();
 
   // Servo initialization
-  Serial.println("Attaching to servo"); 
+  Serial.println("Attaching to servo");
   servo.attach(SERVO_PIN, SERVO_MIN, SERVO_MAX);
 
   // Initialize DHT device
   dht.begin();
 
   // Connect to WiFi
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Connecting to        ");
   lcd.setCursor(0, 1);
   lcd.print(String(ssid) + "                    ");
   Serial.println("Connecting to Wi-Fi");
-  
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
@@ -117,7 +117,7 @@ void setup() {
     Serial.print(".");
     delay(500);
   }
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Wi-Fi connected       ");
   lcd.setCursor(0, 1);
   lcd.print("                      ");
@@ -127,22 +127,22 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP().toString() + "               ");
   delay(2000);
-  
+
   updateNTP();
 
-  for(int i=0; i<8; i++)
+  for (int i = 0; i < 8; i++)
   {
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print(String(i) + " -> " + String(dayPos[i]) + "               ");
     servo.write(dayPos[i]);
     delay(1500);
   }
 
-  for(int i=0; i<8; i++)
+  for (int i = 0; i < 8; i++)
   {
     Serial.println(String(i) + " -> " + String(dayPos[i]));
   }
-  
+
   servo.write(dayPos[0]);
 
   sensor_t sensor;
@@ -156,7 +156,7 @@ void setup() {
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
   Serial.println(F("------------------------------------"));
-  
+
   // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
   Serial.println(F("Humidity Sensor"));
@@ -169,47 +169,47 @@ void setup() {
   Serial.println(F("------------------------------------"));
 
   // Initialize MHZ-19B device
-  mySerial.begin(BAUDRATE); 
+  mySerial.begin(BAUDRATE);
   mhz19.begin(mySerial);
   mhz19.autoCalibration();
 
   // Set delay between sensor readings based on sensor details
   delayMS = 5000; //(sensor.min_delay / 1000) * 2;
   monitoringEndLoops = (24 * 60 * 60) / (delayMS / 1000);
-  
-  #ifdef UDP_HOST_IP
+
+#ifdef UDP_HOST_IP
   // nc -u -l 1234
-  if(udp.connect(IPAddress(UDP_HOST_IP), UDP_HOST_PORT)) {
-      Serial.println("UDP connected");
-      udp.onPacket([](AsyncUDPPacket packet) {
-          Serial.print("UDP Packet Type: ");
-          Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
-          Serial.print(", From: ");
-          Serial.print(packet.remoteIP());
-          Serial.print(":");
-          Serial.print(packet.remotePort());
-          Serial.print(", To: ");
-          Serial.print(packet.localIP()); 
-          Serial.print(":");
-          Serial.print(packet.localPort());
-          Serial.print(", Length: ");
-          Serial.print(packet.length());
-          Serial.print(", Data: ");
-          Serial.write(packet.data(), packet.length());
-          Serial.println();
-          //reply to the client
+  if (udp.connect(IPAddress(UDP_HOST_IP), UDP_HOST_PORT)) {
+    Serial.println("UDP connected");
+    udp.onPacket([](AsyncUDPPacket packet) {
+      Serial.print("UDP Packet Type: ");
+      Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
+      Serial.print(", From: ");
+      Serial.print(packet.remoteIP());
+      Serial.print(":");
+      Serial.print(packet.remotePort());
+      Serial.print(", To: ");
+      Serial.print(packet.localIP());
+      Serial.print(":");
+      Serial.print(packet.localPort());
+      Serial.print(", Length: ");
+      Serial.print(packet.length());
+      Serial.print(", Data: ");
+      Serial.write(packet.data(), packet.length());
+      Serial.println();
+      //reply to the client
 
-          packet.printf("Got %u bytes of data\n", packet.length());
+      packet.printf("Got %u bytes of data\n", packet.length());
 
-      });
-      //Send unicast
-      udp.print("Hello Server at " + String(localTime()) + " [" + String(millis()) + "] from " + WiFi.localIP().toString() + "\n");
-      Serial.println(WiFi.localIP().toString());
+    });
+    //Send unicast
+    udp.print("Hello Server at " + String(localTime()) + " [" + String(millis()) + "] from " + WiFi.localIP().toString() + "\n");
+    Serial.println(WiFi.localIP().toString());
   }
-  #else
+#else
   udp = NULL;
-  #endif
-  
+#endif
+
   webServer.on("/",     HTTP_GET, httpRootHandler);
   webServer.on("/xml",  HTTP_GET, httpXMLHandler);
   webServer.on("/json", HTTP_GET, httpJSONHandler);
@@ -219,32 +219,32 @@ void setup() {
   lcd.print("Starting...             ");
 }
 
-void loop() 
-{  
+void loop()
+{
   unsigned long ticks = millis();
 
-  if((unsigned long)(ticks - sensorDelayTicks) >= delayMS)
+  if ((unsigned long)(ticks - sensorDelayTicks) >= delayMS)
   {
     sensorDelayTicks = ticks;
-    
+
     // Update the LCD with temperature, humidity, and CO2
     print_sensors();
 
     struct tm t;
     getLocalTime(&t);
     move_pointer(t.tm_wday, t.tm_hour);
-    
+
     Serial.println("---------------------------------------------------------------------");
   }
 
   // Update NTP every 6 hours
-  if((unsigned long)(ticks - ntpUpdated) >= 6 * 60 * 60 * 1000)
+  if ((unsigned long)(ticks - ntpUpdated) >= 6 * 60 * 60 * 1000)
   {
     updateNTP();
   }
 
   delay(100);
-} 
+}
 
 void print_sensors()
 {
@@ -254,19 +254,19 @@ void print_sensors()
   bool showMinMax = loops > 0 && !(loops % 5);
   int CO2;
   float t = 0.0, h = 0.0;
-  
+
   Serial.println("print_sensors() [" + String(millis()) + "]");
-  
+
   lastCO2 = CO2 = mhz19.getCO2();
-  Serial.print("    CO2 (ppm): ");                      
+  Serial.print("    CO2 (ppm): ");
   Serial.println(CO2);
 
-  if(CO2 <= 0 || mhz19.errorCode != RESULT_OK)
+  if (CO2 <= 0 || mhz19.errorCode != RESULT_OK)
   {
     co2Failures++;
     Serial.println("    MH-Z19B failure " + String(co2Failures) + " errorCode = " + String(mhz19.errorCode));
     mhz19.verify();
-    if(mhz19.errorCode == RESULT_OK)
+    if (mhz19.errorCode == RESULT_OK)
     {
       co2Failures = 0;
       CO2 = mhz19.getCO2();
@@ -274,7 +274,7 @@ void print_sensors()
     }
     else
     {
-      if(co2Failures >= 3)
+      if (co2Failures >= 3)
       {
         co2Failures = 0;
         Serial.println("    Preforming a reset on CO2 sensor - error_code = " + String(mhz19.errorCode));
@@ -283,16 +283,16 @@ void print_sensors()
       }
       return;
     }
-  } 
+  }
 
-  if(CO2 >= CO2_MIN && CO2 <= CO2_MAX)
+  if (CO2 >= CO2_MIN && CO2 <= CO2_MAX)
   {
     minCO2 = min(minCO2, CO2);
     maxCO2 = max(maxCO2, CO2);
   }
-  
-  Serial.print("    MH-Z19B Temperature (C): ");                  
-  Serial.println(String(mhz19.getTemperature()));                               
+
+  Serial.print("    MH-Z19B Temperature (C): ");
+  Serial.println(String(mhz19.getTemperature()));
   Serial.print("    Min CO2: ");
   Serial.println(minCO2);
   Serial.print("    Max CO2: ");
@@ -302,72 +302,72 @@ void print_sensors()
   dht.temperature().getEvent(&event);
   t = event.temperature;
   Serial.println("    T: " + String(t) + " C / " + String(c_to_f(t)) + " F" );
-  
-  noTemp = isnan(t);  
+
+  noTemp = isnan(t);
   if (noTemp)
   {
     Serial.println(F("    Error reading temperature!"));
-    lcd.setCursor(0, 0); 
+    lcd.setCursor(0, 0);
     lcd.print("Error reading temperature            ");
   }
-  else 
+  else
   {
-    #ifdef DISPLAY_CELSIUS
+#ifdef DISPLAY_CELSIUS
     char c_or_f = 'C';
-    #else
+#else
     char c_or_f = 'F';
-    #endif
-    
+#endif
+
     lastTemperature = t;
     minTemp = min(minTemp, t);
     maxTemp = max(maxTemp, t);
     String msg;
-    if(!showMinMax)
+    if (!showMinMax)
     {
-      msg = "T: " + String(convert_temp(t)) + c_or_f +"  CO2:  ";
+      msg = "T: " + String(convert_temp(t)) + c_or_f + "  CO2:  ";
     }
     else
     {
-      if(showTempMinMax)
+      if (showTempMinMax)
       {
         msg = String(convert_temp(minTemp)) + c_or_f + " / " + String(convert_temp(maxTemp)) + c_or_f + "          ";
       }
       else
       {
         msg = String("CO2 Min: " + String(minCO2) + "                  ");
-      }      
+      }
     }
 
-    lcd.setCursor(0,0);
-    lcd.print(msg);    
+    lcd.setCursor(0, 0);
+    lcd.print(msg);
   }
-  
-  // Get humidity event and print its value.  
+
+  // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
   h = event.relative_humidity;
   Serial.println("    H: " + String(h) + "%");
-  
+
   noHumidity = isnan(h);
-  if (noHumidity) 
+  if (noHumidity)
   {
     Serial.println(F("    Error reading humidity!"));
-    lcd.setCursor(0, 0); 
+    lcd.setCursor(0, 0);
     lcd.print("Error reading humidity                    ");
   }
-  else 
+  else
   {
     lastHumidity = h;
     minHumidity = min(minHumidity, h);
-    maxHumidity = max(maxHumidity, h);    
+    maxHumidity = max(maxHumidity, h);
 
     String msg;
-    if(!showMinMax)
+    if (!showMinMax)
     {
       msg = "H: " + String(h) + "%  " + String(CO2) + "      ";
     }
     else
-    {     
-      if(showTempMinMax)
+    {
+      if (showTempMinMax)
       {
         msg = String(minHumidity) + "% / " + String(maxHumidity) + "%            ";
       }
@@ -378,22 +378,22 @@ void print_sensors()
 
       showTempMinMax = !showTempMinMax;
     }
-    
-    lcd.setCursor(0,1);
+
+    lcd.setCursor(0, 1);
     lcd.print(msg);
 
     lastSensorTimestamp = time(NULL);
     strcpy(lastSensorLocaltime, localTime());
 
-    #ifdef UDP_HOST_IP
-      char buf[1024];
-      to_xml(t, h, CO2, lastSensorTimestamp, buf, sizeof(buf));
-      udp.print(String(buf) + "\n");
-    #endif
+#ifdef UDP_HOST_IP
+    char buf[1024];
+    to_xml(t, h, CO2, lastSensorTimestamp, buf, sizeof(buf));
+    udp.print(String(buf) + "\n");
+#endif
   }
-  
+
   loops += 1;
-  if(loops >= monitoringEndLoops)
+  if (loops >= monitoringEndLoops)
   {
     // Reset the min/max values and loop count
     if (!noTemp)
@@ -405,7 +405,7 @@ void print_sensors()
       minTemp = NO_DATA_HIGH;
       maxTemp = NO_DATA_LOW;
     }
-    
+
     if (!noHumidity)
     {
       minHumidity = maxHumidity = h;
@@ -418,7 +418,7 @@ void print_sensors()
 
     minCO2 = CO2_MAX;
     maxCO2 = CO2_MIN;
-    
+
     loops = 0;
   }
 
@@ -428,19 +428,19 @@ void print_sensors()
 void move_pointer(int wday, int hour)
 {
   Serial.println("move_pointer(" + String(wday) + ", " + String(hour) + ") [" + String(millis()) + "]");
-  
-  if(wday < 0 || wday > 6)
+
+  if (wday < 0 || wday > 6)
     wday = 0;
-  if(hour < 0 || hour > 23)
+  if (hour < 0 || hour > 23)
     hour = 0;
-    
+
   int left = dayPos[wday];
   int right = dayPos[wday + 1];
   float divsPerHour = (left - right) / 24.0;
-  
+
   //Serial.print(("wday = " + String(wday) + "\n").c_str());
   //Serial.print((String(left) + " , " + String(right) + "\n").c_str());
-  
+
   int pos = int(left - (hour * divsPerHour));
   Serial.println(("    " + String(localTime()) + ": Hour = " + String(hour) + " -> Pos = " + String(pos) + " [" + String(left) + " , " + String(right) + "]").c_str());
   servo.write(pos);
@@ -450,22 +450,22 @@ void move_pointer(int wday, int hour)
 
 void updateNTP()
 {
-    Serial.println("Updating time from NTP at [" + String(millis()) + "]");
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-    ntpUpdated = millis();
-    Serial.println("NTP date = " + String(localTime()) + " [" + String(millis()) + "]");
+  Serial.println("Updating time from NTP at [" + String(millis()) + "]");
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  ntpUpdated = millis();
+  Serial.println("NTP date = " + String(localTime()) + " [" + String(millis()) + "]");
 
-    lcd.setCursor(0,0);
-    lcd.print("Updated NTP Time        ");
-    lcd.setCursor(0, 1);
-    lcd.print(String(localTime()));
+  lcd.setCursor(0, 0);
+  lcd.print("Updated NTP Time        ");
+  lcd.setCursor(0, 1);
+  lcd.print(String(localTime()));
 }
 
 char* localTime()
 {
   memset(localTimeBuffer, 0, sizeof(localTimeBuffer));
   struct tm t;
-  if(!getLocalTime(&t)){
+  if (!getLocalTime(&t)) {
     Serial.println("Failed to obtain time");
     updateNTP();
     return localTimeBuffer;
@@ -482,13 +482,13 @@ void to_xml(float temperature, float humidity, int co2, int lastSensorTimestamp,
 
 void to_json(float temperature, float humidity, int co2, int lastSensorTimestamp, char *buf, int len)
 {
-  snprintf(buf, len - 1, "{\"temp_f\": %0.2f, \"temp_c\": %0.2f, \"humidity\": %0.2f, \"co2\": %d, \"timestamp\": %d, \"millis\": %d, \"timestamp_str\": \"%s\"}", c_to_f(temperature), temperature, humidity, co2, lastSensorTimestamp, millis(), lastSensorLocaltime); 
+  snprintf(buf, len - 1, "{\"temp_f\": %0.2f, \"temp_c\": %0.2f, \"humidity\": %0.2f, \"co2\": %d, \"timestamp\": %d, \"millis\": %d, \"timestamp_str\": \"%s\"}", c_to_f(temperature), temperature, humidity, co2, lastSensorTimestamp, millis(), lastSensorLocaltime);
 }
 
 void httpRootHandler(AsyncWebServerRequest *request)
 {
   Serial.println("HTTP request from " + request->client()->remoteIP().toString() + ": / [" + String(millis()) + "]");
-   
+
   const char *html = R"(
 <html>
    <head>
